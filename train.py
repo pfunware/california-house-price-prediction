f79ccaa7abeba413b5d00a96b2502d542819c56b
Tim Pfuntner <tim.pfuntner@gmail.com>
2026-04-11 00:55:35 +0530

    housing csv

--- train.py
+++ train.py
@@ -1,16 +1,14 @@
 import os
 import pandas as pd
-from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from pathlib import Path
 import joblib
+
 def main():
-    data = fetch_california_housing()
-    X = pd.DataFrame(data.data, columns=data.feature_names)
-    y = data.target
-    # print(X.head())
-    # print(y)
+    df = pd.read_csv("./data/housing.csv")
+    print(df.head())
+    X = df.drop("target", axis=1)
+    y = df["target"]
     X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
 
     # Train
@@ -18,10 +16,9 @@ def main():
     model.fit(X_train,y_train)
 
     # Save the model
-    Path("artifacts").mkdir(parents=True, exist_ok=True)
+    os.mkdir("artifacts")
     model_path = os.path.join("artifacts", "model.pkl")
     joblib.dump(model, model_path)
-
 
 if __name__ == '__main__':
     main()
